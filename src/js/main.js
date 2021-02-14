2e8b98bcf822b33b2e1632cb1f312e0ec16fa3be
ksilveria86 <kate.silveria86@gmail.com>
2021-02-13 20:19:53 -0800

    Responsiveness, AOS animations

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,18 @@
 document.addEventListener('DOMContentLoaded', function(e) {
+    AOS.init({duration: 800});
+
     let footerYear = document.querySelector('footer span.year');
     if (footerYear) {
         let today = new Date();
         footerYear.innerText = today.getFullYear();
     }
+    
+    // Nav
+    let mobileNav = document.querySelector('nav.mobile-nav .slideout');
+    document.querySelector('nav.mobile-nav .hamburger').addEventListener('click', function(e) {
+        mobileNav.classList.add('open');
+    })
+    mobileNav.querySelector('.close').addEventListener('click', function(e) {
+        mobileNav.classList.remove('open');
+    })
 })
